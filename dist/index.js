910def775bc67ecc93a5ef954aedf16b9c324fa4
iruzki <iru.arostegui@gmail.com>
2021-03-26 22:24:16 -0300

    build

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62637" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "63064" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
