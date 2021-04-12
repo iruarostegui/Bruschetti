9fbb57d6b8d5491c3248c5a6424bffd79d63fab2
iruzki <iru.arostegui@gmail.com>
2021-04-12 11:46:31 -0300

    mail fix

--- src/mail.php
+++ src/mail.php
@@ -2,7 +2,7 @@
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
         # FIX: Replace this email with recipient email
-        $mail_to = "info@bruschetti.com.ar";
+        $mail_to = "redes@bruschetti.com.ar";
         
         # Sender Data
         $subject = trim($_POST["subject"]);
